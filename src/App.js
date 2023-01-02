783104de36abf5c2009723dc691fe6380eca3ec1
AC <chenyibo416@gmail.com>
2023-01-02 13:48:18 +0800

    add clear all function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
+  const [people, setPeople] = useState(data);
+
+  function clearAllRecords() {
+    setPeople([]);
+  }
   return (
     <main>
       <section className="container">
-        <h3>{data.length} Birthday today</h3>
-        <List people={data} />
+        <h3>{people.length} Birthday today</h3>
+        <List people={people} />
+        <button onClick={clearAllRecords}>clear All</button>
       </section>
     </main>
   );
